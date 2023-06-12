fb81ab58663bae5f5655706addd9966c01982643
Dajve Echols <dajvelechols@gmail.com>
2023-06-12 17:00:46 -0500

    grab data from excel sheet

--- job-tracker/src/component/FileDownload.tsx
+++ job-tracker/src/component/FileDownload.tsx
@@ -1,25 +1,30 @@
-import { Button } from "@chakra-ui/button";
-import { useForm } from "react-hook-form";
+import { read } from "xlsx";
 
 const FileDownload = () => {
-  const { register, handleSubmit } = useForm();
-
-  const onSubmit = async (data: any) => {
-    console.log(data)
+  const onSubmit = async (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = function (e) {
+        const data = new Uint8Array(e.target?.result as ArrayBuffer);
+        const workbook = read(data, { type: "array" });
+        console.log(workbook);
+      };
+      reader.readAsArrayBuffer(file);
+    }
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
+    <>
       <label htmlFor="excel">Choose Excel Sheet:</label>
       <input
-        {...register("excel")}
+        onChange={(event) => onSubmit(event)}
         type="file"
         id="excel"
         name="excel"
         accept=".xlsx"
       />
-      <Button type="submit">Submit</Button>
-    </form>
+    </>
   );
 };
 
