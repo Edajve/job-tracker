21b780adaef3999a6ce9470039c31e157a974c62
Dajve Echols <edajve@gmail.com>
2023-06-12 17:15:12 -0500

    clean up code
    Clean up some unused references

--- job-tracker/src/App.tsx
+++ job-tracker/src/App.tsx
@@ -1,23 +1,23 @@
-import { Text, Grid, GridItem } from "@chakra-ui/layout";
+import { Grid, GridItem } from "@chakra-ui/layout";
 import { Show } from "@chakra-ui/react";
 import "./App.css";
 import NavBar from "./component/NavBar";
 
 function App() {
   return (
-      <Grid
-        templateAreas={{
-          base: `"nav" "main"`,
-          lg: `"nav nav" "aside main"`, //lg = larger than 1024px
-        }}
-        templateColumns={{
-          base: "1fr",
-          lg: "200px 1fr",
-        }}>
-        <GridItem background="blue.200" as="nav" height="80px">
-          <NavBar />
-        </GridItem>
-        <Show above="lg">
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`, //lg = larger than 1024px
+      }}
+      templateColumns={{
+        base: "1fr",
+        lg: "200px 1fr",
+      }}>
+      <GridItem background="blue.200" as="nav" height="80px">
+        <NavBar />
+      </GridItem>
+      <Show above="lg">
         <GridItem background="green.200" area="aside" paddingX={5}>
           Aside
         </GridItem>
@@ -25,7 +25,7 @@ function App() {
       <GridItem background="orange.200" area="main">
         main
       </GridItem>
-      </Grid>
+    </Grid>
   );
 }
 
--- job-tracker/src/main.tsx
+++ job-tracker/src/main.tsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { ChakraProvider, ColorModeScript, theme } from '@chakra-ui/react'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { ChakraProvider, ColorModeScript, theme } from "@chakra-ui/react";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <ChakraProvider  theme={theme}>
-    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <App />
     </ChakraProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
