d970b1033b8f91f29752cedc35259f26cc905a22
Dajve Echols <edajve@gmail.com>
2023-06-13 21:33:44 -0500

    added dynamic header

--- job-tracker/src/component/ApplicationLists.tsx
+++ job-tracker/src/component/ApplicationLists.tsx
@@ -10,12 +10,26 @@ import {
 } from "@chakra-ui/react";
 import ApplicationListPages from "./ApplicationListPages";
 
-const ApplicationLists = () => {
+interface Props {
+  headers: (data: any) => void;
+}
+
+const ApplicationLists = ({ headers }: Props) => {
   const amountOfHeaders = [
     1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18,
   ];
   const amountOfRowsInAList = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   const amountOfColumnsInAList = amountOfHeaders;
+
+  const headerToArray = () => {
+    var header = [];
+    if (headers) {
+      for (const [key, value] of Object.entries(headers)) {
+        header.push(value);
+      }
+    }
+    return header;
+  };
 
   return (
     <Box height="300px">
@@ -23,8 +37,8 @@ const ApplicationLists = () => {
         <Table variant="simple">
           <Thead>
             <Tr>
-              {amountOfHeaders.map((header, index) => (
-                <Th key={index}>row {header}</Th>
+              {headerToArray().map((header, index) => (
+                <Th key={index}>{header}</Th>
               ))}
             </Tr>
           </Thead>
