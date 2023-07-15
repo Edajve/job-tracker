c89a01a0f8e3854ed6fc31c45ed1284c41327eae
Dajve Echols <edajve@gmail.com>
2023-07-15 08:43:05 -0500

    fix some ui issues

--- job-tracker/src/component/Aside.tsx
+++ job-tracker/src/component/Aside.tsx
@@ -77,6 +77,7 @@ const Aside = ({
       {deleteBtn && (
         <ListItem>
           <ConfirmDeleteApplication
+            indexOfApplicationBeingDeleted={indexOfApplication}
             setDeleteFlag={(bool) => setSureToDelete(bool)}
           />
         </ListItem>
--- job-tracker/src/component/ConfirmDeleteApplication.tsx
+++ job-tracker/src/component/ConfirmDeleteApplication.tsx
@@ -2,9 +2,10 @@ import { Box, Text, Button } from "@chakra-ui/react";
 
 interface Props {
   setDeleteFlag: (bool: boolean) => void;
+  indexOfApplicationBeingDeleted: string;
 }
 
-const ConfirmDeleteApplication = ({ setDeleteFlag }: Props) => {
+const ConfirmDeleteApplication = ({ setDeleteFlag, indexOfApplicationBeingDeleted }: Props) => {
   const handleClick = (
     yesOrNo: React.MouseEvent<HTMLButtonElement, MouseEvent>
   ) => {
@@ -17,17 +18,19 @@ const ConfirmDeleteApplication = ({ setDeleteFlag }: Props) => {
   return (
     <Box
       padding={4}
-      width="200px"
-      height="130px"
+      width="230px"
+      height="150px"
       border="2px solid gray"
       borderRadius={10}>
-      <Text>You sure you want to delete this?</Text>
-      <Button margin={2} color="green" onClick={(item) => handleClick(item)}>
-        Yes
-      </Button>
-      <Button color="red" onClick={(item) => handleClick(item)}>
-        No
-      </Button>
+      <Text>You sure you want to delete application id: {indexOfApplicationBeingDeleted}?</Text>
+      <Box mt={3}>
+        <Button margin={2} marginLeft='30px' color="green" onClick={(item) => handleClick(item)}>
+          Yes
+        </Button>
+        <Button color="red" onClick={(item) => handleClick(item)}>
+          No
+        </Button>
+      </Box>
     </Box>
   );
 };
